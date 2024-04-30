452db93ec026faf05173fb0bcb6d6d1633babd41
Imamsha <imamshas125@gmail.com>
2024-04-30 14:26:55 +0530

    fix path issue

---
+++ src/stores/components/AC.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { acData } from "../data/ac";
+import { Link } from "react-router-dom";
+
+const AC = () => {
+  const firstFiveImages = acData.slice(0, 5);
+
+  useEffect(() => {
+    fetch('http://192.168.1.163:8093/admin/ac')
+    .then(response => response.json())
+
+
+
+    
+  
+  },[])
+
+  return (
+    <>
+      <div className="proTitle">
+        <h2>Air Condition</h2>
+      </div>
+      <div className="proSection">
+        {firstFiveImages.map((item) => {
+          return (
+            <div className="imgBox">
+              <Link to='/ac'>
+                <img className="proImage" src={item.image} alt="" />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default AC;
